e0c7931d8c05d3a45f4c67f83e82838cb62cf641
eqiii <muhammadrizqiarroyyansmktibazm@gmail.com>
2025-08-28 13:43:36 +0700

    first commit

---
+++ Dummy/dummy/src/app.jsx
@@ -0,0 +1,13 @@
+import DataFetcher from './effek';
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Data dari API</h1>
+      <DataFetcher />
+    </div>
+  );
+}
+
+export default App;
---
+++ Dummy/dummy/src/counter.jsx
@@ -0,0 +1,22 @@
+// import React, { useState } from 'react';
+
+
+// function Counter() {
+//   // Membuat state 'count' dengan nilai awal 0
+//   const [count, setCount] = useState(0);
+
+//   // Fungsi untuk menambah angka
+//   const tambah = () => setCount(count + 1);
+//    // Fungsi untuk mengurangi angka
+//   const kurang = () => setCount(count - 1);
+
+//   return (
+//     <div>
+//       <h1>Hitungan: {count}</h1>
+//       <button onClick={tambah}>Tambah</button>
+//       <button onClick={kurang}>Kurang</button>
+//     </div>
+//   );
+// }
+
+// export default Counter;
